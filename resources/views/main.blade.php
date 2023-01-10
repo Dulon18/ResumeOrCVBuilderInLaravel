f5020b0f33c0004429c291700f373d72d2da4052
Dulon18 <dulon587@gmail.com>
2023-01-10 22:08:45 +0600

    made home page and db

---
+++ resources/views/main.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+<h2>Welcome To CV / Resume Builder</h2>
+<button class="btn btn-info">Build Your Resume Now</button>
+</div>
+
+@endsection
