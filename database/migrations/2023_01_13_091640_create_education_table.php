d7a84bec5b5ceafd3834e29d35128fc7463ffd11
Dulon18 <dulon587@gmail.com>
2023-01-13 15:38:27 +0600

    add education section

---
+++ database/migrations/2023_01_13_091640_create_education_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('education', function (Blueprint $table) {
+            $table->id();
+            $table->string('graduation_college_name');
+            $table->string('graduation_college_location');
+            $table->string('graduation_field_of_study');
+            $table->string('graduation_cgpa');
+            $table->string('graduation_college_start_year');
+            $table->string('graduation_college_end_year');
+            $table->string('hsc_college_name');
+            $table->string('hsc_college_location');
+            $table->string('hsc_field_of_study');
+            $table->string('hsc_gpa');
+            $table->string('hsc_college_start_year');
+            $table->string('hsc_college_end_year');
+            $table->string('ssc_school_name');
+            $table->string('ssc_school_location');
+            $table->string('ssc_field_of_study');
+            $table->string('ssc_gpa');
+            $table->string('ssc_school_start_year');
+            $table->string('ssc_school_end_year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('education');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserDetailsController;
+use App\Http\Controllers\EducationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,7 +25,15 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 //user details route start
 
-Route::get('user-details/create',[UserDetailsController::class,'create'])->name('user_details.create')->middleware('auth');
-Route::post('user-details/store',[UserDetailsController::class,'store'])->name('user_details.store')->middleware('auth');
+Route::get('user-details/create',[UserDetailsController::class,'create'])->middleware('auth')->name('user_details.create');
+Route::post('user-details/store',[UserDetailsController::class,'store'])->middleware('auth')->name('user_details.store');
 
 //user details route end
+
+
+//user Education route start
+
+Route::get('user-edu/create',[EducationController::class,'create'])->name('user_education.create')->middleware('auth');
+//Route::post('user-details/store',[UserDetailsController::class,'store'])->name('user_details.store')->middleware('auth');
+
+//user Education route end
